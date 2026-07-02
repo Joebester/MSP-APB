4d10c2a23412a4f344dda51c626aa4ebe419c8bd
MTonz <tonvnp2002@gmail.com>
2026-07-02 09:02:00 +0700

    all UI ready

--- src/components/registration/DocumentCaptureCard.jsx
+++ src/components/registration/DocumentCaptureCard.jsx
@@ -1,40 +1,27 @@
 import { Camera, ChevronRight, Video } from 'lucide-react';
 import { Button } from '../ui/Button';
+import Picture_Icon from '../../assets/icons/ic_kyc_1.png';
+import PicVid_Icon from '../../assets/icons/ic_kyc_2.png';
 
-function CornerBrackets() {
-  return (
-    <>
-      <span className="absolute left-3 top-3 h-5 w-5 border-l-2 border-t-2 border-teal-500" />
-      <span className="absolute right-3 top-3 h-5 w-5 border-r-2 border-t-2 border-teal-500" />
-      <span className="absolute bottom-3 left-3 h-5 w-5 border-b-2 border-l-2 border-teal-500" />
-      <span className="absolute bottom-3 right-3 h-5 w-5 border-b-2 border-r-2 border-teal-500" />
-    </>
-  );
-}
+// function CornerBrackets() {
+//   return (
+//     <>
+//       <span className="absolute left-3 top-3 h-5 w-5 border-l-2 border-t-2 border-teal-500" />
+//       <span className="absolute right-3 top-3 h-5 w-5 border-r-2 border-t-2 border-teal-500" />
+//       <span className="absolute bottom-3 left-3 h-5 w-5 border-b-2 border-l-2 border-teal-500" />
+//       <span className="absolute bottom-3 right-3 h-5 w-5 border-b-2 border-r-2 border-teal-500" />
+//     </>
+//   );
+// }
 
 function DocumentIllustration({ variant = 'document' }) {
   return (
-    <div className="relative mx-auto flex h-28 w-40 items-center justify-center">
-      <CornerBrackets />
+    <div className="relative mx-auto flex items-center justify-center">
+      {/* <CornerBrackets /> */}
       {variant === 'document' ? (
-        <div className="flex h-16 w-24 flex-col overflow-hidden rounded-md border-2 border-gray-300 bg-white shadow-sm">
-          <div className="h-6 bg-msp-green/20" />
-          <div className="flex flex-1 items-center justify-center p-2">
-            <div className="h-8 w-8 rounded-full bg-gray-200" />
-          </div>
-        </div>
+        <img src={Picture_Icon} alt="Document Illustration" className="h-auto w-40" />
       ) : (
-        <div className="flex items-end gap-2">
-          <div className="flex h-14 w-10 flex-col items-center justify-end rounded-full bg-gray-200 pb-1">
-            <div className="h-6 w-6 rounded-full bg-gray-300" />
-          </div>
-          <div className="flex h-12 w-16 flex-col overflow-hidden rounded border-2 border-gray-300 bg-white">
-            <div className="h-3 bg-msp-green/20" />
-            <div className="flex flex-1 items-center justify-center">
-              <div className="h-4 w-4 rounded-full bg-gray-200" />
-            </div>
-          </div>
-        </div>
+        <img src={PicVid_Icon} alt="Document Illustration" className="h-auto w-40" />
       )}
     </div>
   );
