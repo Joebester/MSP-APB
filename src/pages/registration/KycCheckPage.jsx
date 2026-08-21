f34ae58e0c006621b85ae15c25527ae73f68cf30
n7ii <phongsavanh90lvl@gmail.com>
2026-08-21 17:06:18 +0700

    update kyc -check page

--- src/pages/registration/KycCheckPage.jsx
+++ src/pages/registration/KycCheckPage.jsx
@@ -138,17 +138,6 @@ export default function KycCheckPage() {
               <div className="h-px w-12 bg-white/20" />
             </div>
           </div>
-
-          <div className="pb-4 pt-8">
-            <Button
-              variant="white"
-              size="lg"
-              className="w-full gap-1 font-bold text-teal-800"
-              onClick={handleFinish}
-            >
-              <Trans>Done</Trans> <span aria-hidden="true">&gt;</span>
-            </Button>
-          </div>
         </div>
       </GradientBackground>
     );
